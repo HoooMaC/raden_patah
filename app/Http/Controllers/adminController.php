7983ce8bcac15918ba0054132c11dbaa62c1015a
HoooMaC <wawcam123@gmail.com>
2023-11-15 13:49:10 +0700

    memperbaiki argumen pada view function

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -6,20 +6,20 @@ use Illuminate\Http\Request;
 
 class adminController extends Controller
 {
-    
+
     public function index(){
         return view('dashboard/index');
     }
 
     public function program_mingguan(){
-        return view('dashboard/programmingguan');
+        return view('dashboardAdmin/programmingguan');
     }
 
     public function program_triwulan(){
-        return view('dashboard/programtriwulan');
+        return view('dashboardAdmin/programtriwulan');
     }
 
     public function program_insidental(){
-        return view('dashboard/programinsidental');
+        return view('dashboardAdmin/programinsidental');
     }
 }
