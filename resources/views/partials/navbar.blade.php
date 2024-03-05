5aae26f6f57a79504300b95ddc7469dbbb468e4a
HoooMaC <wawcam123@gmail.com>
2024-03-05 20:17:48 +0700

    memperbaiki link home pada navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-    <a href="{{ url('/home') }}" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+    <a href="{{ url('/') }}" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
         <h2 class="m-0 text-primary"><i class=""></i>Masjid Raden Patah</h2>
     </a>
 
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav me-4 ms-auto p-4 p-lg-0 d-flex align-items-center">
-            <a href="{{ url('/home') }}"
+            <a href="{{ url('/') }}"
                 class="nav-item nav-link {{ Request::path() === 'home' ? 'active' : '' }}">Profil</a>
             <a href="{{ url('/program') }}"
                 class="nav-item nav-link {{ Request::path() === 'program' ? 'active' : '' }}">Program</a>
