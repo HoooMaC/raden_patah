c644e4b4533311f9acc9a32ba8757b2b4e0161bf
MHilmiAdz <103296966+MHilmiAdz@users.noreply.github.com>
2024-03-13 20:58:11 +0700

    Update Footer Responsive

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,20 +1,20 @@
 <footer class="mt-5 text-center bg-dark text-white footer wow fadeIn" data-wow-delay="0.1s">
     <div class="container pt-5 pb-1">
-        <div class="d-flex justify-content-center">
-            <a class="btn btn-outline-light btn-social" href="https://wa.me/+628993117777"><i class="fab fa-whatsapp" ></i></a>
-            <p class="btn text-white">0899 3117 777 </p>
-            <a class="btn btn-outline-light btn-social" href="https://www.youtube.com/@MRPMedia"><i class="fab fa-youtube"></i></a>
-            <a class="btn btn-outline-light btn-social" href="#"><i class="fa-brands fa-line"></i></a>
-            <p class="btn text-white">mrpmedia</p>
-            <a class="btn btn-outline-light btn-social" href="https://t.me/mrpmedia"><i class="fab fa-telegram"></i></a>
-            <a class="btn btn-outline-light btn-social" href="https://www.facebook.com/masjidradenpatah.ub"><i class="fab fa-facebook-f"></i></a>
-            <a class="btn btn-outline-light btn-social" href="https://instagram.com/masjidradenpatah.ub"><i class="fab fa-instagram"></i></a>
-            <a class="btn btn-outline-light btn-social" href="https://www.tiktok.com/@masjidradenpatah.ub"><i class="fab fa-tiktok"></i></a>
-            <p class="btn text-white">masjidradenpatah.ub</p>
+        <div class="d-flex flex-wrap justify-content-center align-items-center">
+            <a class="btn btn-outline-light btn-social mx-1" href="https://wa.me/+628993117777"><i class="fab fa-whatsapp"></i></a>
+            <p class="p-2 mt-3 text-white">0899 3117 777 </p>
+            <a class="btn btn-outline-light btn-social mx-1" href="https://www.youtube.com/@MRPMedia"><i class="fab fa-youtube"></i></a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#"><i class="fa-brands fa-line"></i></a>
+            <p class="p-2 mt-3 text-white">mrpmedia</p>
+            <a class="btn btn-outline-light btn-social mx-1" href="https://t.me/mrpmedia"><i class="fab fa-telegram"></i></a>
+            <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/masjidradenpatah.ub"><i class="fab fa-facebook-f"></i></a>
+            <a class="btn btn-outline-light btn-social mx-1" href="https://instagram.com/masjidradenpatah.ub"><i class="fab fa-instagram"></i></a>
+            <a class="btn btn-outline-light btn-social mx-1" href="https://www.tiktok.com/@masjidradenpatah.ub"><i class="fab fa-tiktok"></i></a>
+            <p class="p-2 mt-3 text-white">masjidradenpatah.ub</p>
         </div>
         <hr>
         <div>
-            <p> <b class="text-white-bold">Copyright &copy;2023, Designed by</b> Digital Creative MRP UB</p>
+            <p><b class="text-white-bold">Copyright &copy;2023, Designed by</b> Digital Creative MRP UB</p>
         </div>
     </div>
 </footer>
