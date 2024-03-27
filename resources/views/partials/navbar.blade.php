0793de11a5266ac8c88f4223d7a81adea66d3961
HoooMaC <wawcam123@gmail.com>
2024-03-27 23:37:53 +0700

    memasukkan navbar ke container

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,17 +1,21 @@
 <nav class="mrp-navbar">
-    <a href="{{ url('/') }}" class="mrp-navbar__logo">
-        <h2>Masjid Raden Patah</h2>
-    </a>
+    <div class="mrp-container">
+        <a href="{{ url('/') }}" class="mrp-navbar__logo">
+            <h2>Masjid Raden Patah</h2>
+        </a>
 
-    <div class="mrp-navbar__list" id="navbarCollapse">
-        <a href="{{ url('/') }}" class="mrp-navbar__link {{ Request::path() === '/' ? 'active' : '' }}">Profil</a>
-        <a href="{{ url('/program') }}"
-            class="mrp-navbar__link {{ Request::path() === 'program' ? 'active' : '' }}">Program</a>
-        <a href="{{ url('/unit') }}" class="mrp-navbar__link {{ Request::path() === 'unit' ? 'active' : '' }}">Unit</a>
-        <a href="{{ url('/layanan') }}"
-            class="mrp-navbar__link {{ Request::path() === 'layanan' ? 'active' : '' }}">Layanan</a>
-        {{-- TEMPORARY Inactive for now --}}
-        {{-- <a href="{{ url('/signin') }}" class="btn btn-primary h-50">Login</a> --}}
+        <div class="mrp-navbar__list" id="navbarCollapse">
+            <a href="{{ url('/') }}"
+                class="mrp-navbar__link {{ Request::path() === '/' ? 'active' : '' }}">Profil</a>
+            <a href="{{ url('/program') }}"
+                class="mrp-navbar__link {{ Request::path() === 'program' ? 'active' : '' }}">Program</a>
+            <a href="{{ url('/unit') }}"
+                class="mrp-navbar__link {{ Request::path() === 'unit' ? 'active' : '' }}">Unit</a>
+            <a href="{{ url('/layanan') }}"
+                class="mrp-navbar__link {{ Request::path() === 'layanan' ? 'active' : '' }}">Layanan</a>
+            {{-- TEMPORARY Inactive for now --}}
+            {{-- <a href="{{ url('/signin') }}" class="btn btn-primary h-50">Login</a> --}}
+        </div>
+        <i class="mrp-navbar__bars  fa-solid fa-bars"></i>
     </div>
-    <i class="mrp-navbar__bars  fa-solid fa-bars"></i>
 </nav>
