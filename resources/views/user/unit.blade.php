12545106dd4456065ca013982105edff07209073
HoooMaC <wawcam123@gmail.com>
2024-03-28 11:23:02 +0700

    menambahkan deskripsi pada halaman unit

--- resources/views/user/unit.blade.php
+++ resources/views/user/unit.blade.php
@@ -25,15 +25,11 @@
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
                     {{-- <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6> // --}}
                     <h2 class="mb-4">UMAR (Unit Mahasiswa Masjid Raden Patah) </h2>
-                    <p class="mb-4" style="text-align: justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe impedit inventore
-                        corrupti quae eius animi necessitatibus repellat molestiae aspernatur pariatur eligendi libero
-                        eveniet quas doloremque facilis voluptatem id, consequuntur, deleniti quidem dolores, voluptates
-                        modi! Non repellat porro dicta voluptatibus, sit, totam quasi reprehenderit alias nam culpa magni
-                        veniam beatae suscipit expedita deserunt necessitatibus quibusdam perspiciatis ad temporibus
-                        corporis quas. Eius possimus, nostrum, omnis consectetur similique nam quam quaerat soluta
-                        architecto accusantium temporibus eligendi repellat, aut harum dolore deleniti perspiciatis iste
-                        aperiam unde sint quis porro eos error perferendis. Aliquam, molestiae libero! Autem esse quaerat
-                        sed nisi sit earum soluta expedita.</p>
+                    <p class="mb-4" style="text-align: justify">
+                        Umar adalah unit mahasiswa di Masjid Raden Patah yang berfokus pada eksekutor program-program yang
+                        ada di masjid.
+                        Ada banyak program yang dijalankan seperti kismala, kultum zuhur, kelas pranikah dan parenting, dll.
+                    </p>
                     {{-- <button type="button" class="btn btn-primary" style="border-radius: 10px">See More</button> --}}
                 </div>
             </div>
@@ -50,18 +46,16 @@
                 <div class="col-lg-6 order-last order-md-first wow fadeInUp" data-wow-delay="0.3s">
                     <h2 class="mb-4">PTQ (Pusat Tahfidz Qur'an) </h2>
                     <p class="mb-4" style="text-align: justify">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quis distinctio quos quam deserunt
-                        aliquam iusto quaerat, facilis provident consectetur unde cupiditate sint ipsa, repellat eius ipsum,
-                        reiciendis aperiam sapiente eveniet? Rerum mollitia, magni placeat doloremque qui labore dolorem
-                        distinctio natus molestiae saepe voluptate, dicta repellendus. Similique voluptatum iusto inventore
-                        cumque est, fuga soluta velit unde aut consequatur placeat ipsa ipsum asperiores illum fugit
-                        suscipit, nemo libero, quaerat repellendus quis nostrum? Impedit autem itaque enim eius fugiat!
-                        Doloribus nemo porro quidem amet minus sit illo magnam eos, laudantium atque dolorem, ducimus
-                        accusamus voluptatibus ipsum rerum ex fuga incidunt ea? Maxime!
+                        PTQ Adalah unit di Masjid Raden Patah yang berfokus kepada Al-Qur'an. Khususnya program tahfidz.
+                        Program tahfidz yang ada di PTQ terbuka untuk umum dan bisa diikuti oleh siapa saja.
+                        Selain program tahfidz, PTQ juga mengadakan program baca tulis Al-Qur'an, kelas tahsin, kajian
+                        tafsir,
+                        tahfidz camp, wisuda tahfidz, dan lain-lain.
                     </p>
                     {{-- <button type="button" class="btn btn-primary" style="border-radius: 10px">See More</button> --}}
                 </div>
-                <div class="col-lg-6 order-first order-md-last wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
+                <div class="col-lg-6 order-first order-md-last wow fadeInUp" data-wow-delay="0.1s"
+                    style="min-height: 400px;">
                     <div class="position-relative h-75" style="display:flex; align-items:center; justify-content:center;">
                         <img class="img-fluid position-absolute" style="max-height: 100%;"
                             src="{{ asset('logo_unit/Logo_PTQ.png') }}" alt="logo ptq" style="object-fit: cover;">
@@ -69,7 +63,7 @@
                 </div>
             </div>
         </div>
-    </div>    
+    </div>
 
     <!-- PTQ End -->
 
@@ -88,15 +82,13 @@
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
                     {{-- <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6> // --}}
                     <h2 class="mb-4">DC (Digital Creative)</h2>
-                    <p class="mb-4" style="text-align: justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe impedit inventore
-                        corrupti quae eius animi necessitatibus repellat molestiae aspernatur pariatur eligendi libero
-                        eveniet quas doloremque facilis voluptatem id, consequuntur, deleniti quidem dolores, voluptates
-                        modi! Non repellat porro dicta voluptatibus, sit, totam quasi reprehenderit alias nam culpa magni
-                        veniam beatae suscipit expedita deserunt necessitatibus quibusdam perspiciatis ad temporibus
-                        corporis quas. Eius possimus, nostrum, omnis consectetur similique nam quam quaerat soluta
-                        architecto accusantium temporibus eligendi repellat, aut harum dolore deleniti perspiciatis iste
-                        aperiam unde sint quis porro eos error perferendis. Aliquam, molestiae libero! Autem esse quaerat
-                        sed nisi sit earum soluta expedita.</p>
+                    <p class="mb-4" style="text-align: justify">
+                        Digital Creative adalah unit yang ada di Masjid Raden Patah yang berfokus kepada pengembangan media
+                        digital.
+                        Mulai dari desain, fotografi, videografi, sosial media, dan juga pengembangan web. Digital Creative
+                        bertujuan
+                        untuk mengembangkan dakwah dalam bentuk digital.
+                    </p>
                     {{-- <button type="button" class="btn btn-primary" style="border-radius: 10px">See More</button> --}}
                 </div>
             </div>
@@ -113,19 +105,24 @@
                 <div class="col-lg-6 order-last order-md-first wow fadeInUp" data-wow-delay="0.3s">
                     <h2 class="mb-4">IMC (Imam Muda Center)</h2>
                     <p class="mb-4" style="text-align: justify">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quis distinctio quos quam deserunt
-                        aliquam iusto quaerat, facilis provident consectetur unde cupiditate sint ipsa, repellat eius ipsum,
-                        reiciendis aperiam sapiente eveniet? Rerum mollitia, magni placeat doloremque qui labore dolorem
-                        distinctio natus molestiae saepe voluptate, dicta repellendus. Similique voluptatum iusto inventore
-                        cumque est, fuga soluta velit unde aut consequatur placeat ipsa ipsum asperiores illum fugit
-                        suscipit, nemo libero, quaerat repellendus quis nostrum? Impedit autem itaque enim eius fugiat!
-                        Doloribus nemo porro quidem amet minus sit illo magnam eos, laudantium atque dolorem, ducimus
-                        accusamus voluptatibus ipsum rerum ex fuga incidunt ea? Maxime!
+                        Imam Muda Center merupakan unit yang bergerak dibidang pembinaan kader imam muda di Universitas
+                        Brawijaya yang nantinya dibimbing langsung oleh ahli dibidangnya dengan program program menarik yang
+                        ditawarkan. Imam Muda merupakan wadah bagi ahasiswa muslim Universitas Brawijaya yang
+                        ingin berkontribusi dalam kegiatan beribadatan dan mengembangkan potensi menjadi imam sholat untuk
+                        bergabung ke salah satu unit binaannya yaitu Imam Muda Center.
+                    </p>
+                    <p style="text-align: justify">
+                        Ada 2 program yang dilaksanakan oleh Imam Muda Center yaitu: 
+                        <ol>
+                            <li>Kelas seni baca Al-Qur'an</li>
+                            <li>Pembinaan Imam Muda</li>
+                        </ol>
                     </p>
                     {{-- <button type="button" class="btn btn-primary" style="border-radius: 10px">See More</button> --}}
                     {{-- <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a> --}}
                 </div>
-                <div class="col-lg-6 order-first order-md-last wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
+                <div class="col-lg-6 order-first order-md-last wow fadeInUp" data-wow-delay="0.1s"
+                    style="min-height: 400px;">
                     <div class="position-relative h-100" style="display:flex; align-items:center; justify-content:center;">
                         <img class="img-fluid position-absolute" style="max-height: 100%"
                             src="{{ asset('logo_unit/Logo_IMC.png') }}" alt="logo imam muda center"
@@ -153,15 +150,10 @@
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
                     {{-- <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6> // --}}
                     <h2 class="mb-4">MRP Lunch Space</h2>
-                    <p class="mb-4" style="text-align: justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe impedit inventore
-                        corrupti quae eius animi necessitatibus repellat molestiae aspernatur pariatur eligendi libero
-                        eveniet quas doloremque facilis voluptatem id, consequuntur, deleniti quidem dolores, voluptates
-                        modi! Non repellat porro dicta voluptatibus, sit, totam quasi reprehenderit alias nam culpa magni
-                        veniam beatae suscipit expedita deserunt necessitatibus quibusdam perspiciatis ad temporibus
-                        corporis quas. Eius possimus, nostrum, omnis consectetur similique nam quam quaerat soluta
-                        architecto accusantium temporibus eligendi repellat, aut harum dolore deleniti perspiciatis iste
-                        aperiam unde sint quis porro eos error perferendis. Aliquam, molestiae libero! Autem esse quaerat
-                        sed nisi sit earum soluta expedita.</p>
+                    <p class="mb-4" style="text-align: justify">
+                        MRP Lunch Space adalah unit kantin yang ada di Masjid Raden Patah.
+                        Yang menyediakan berbagai makanan yang sehat, halal, dan murah.
+                    </p>
                     {{-- TEMPORARY  Inactive for now --}}
                     {{-- <button type="button" class="btn btn-primary" style="border-radius: 10px">See More</button> --}}
                 </div>
