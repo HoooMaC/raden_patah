d0e996284156d83e2bd0e47f8aa4b25173e38fd3
HoooMaC <wawcam123@gmail.com>
2024-05-12 19:34:07 +0700

    merubah carousel sedikit

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -2,7 +2,7 @@
 
     <div class="mrp-slider">
       <div class="mrp-slider__item active">
-        <img src="{{ asset('img/carousel/footage_mesjid_6.JPG') }}" class="mrp-slider__item-image active">
+        <img src="{{ asset('img/MRP.png') }}" class="mrp-slider__item-image active">
         <div class="mrp-slider__item-desc">
           <h5 class="mrp-slider__item-title">Selamat Datang di Masjid Raden Patah</h5>
           <h6 class="mrp-slider__item-subtitle">Universitas Brawijaya</h6>
@@ -10,15 +10,7 @@
       </div>
 
       <div class="mrp-slider__item">
-        <img src="{{ asset('img/carousel/footage_mesjid_10.png') }}" class="mrp-slider__item-image">
-        <div class="mrp-slider__item-desc">
-          <h5 class="mrp-slider__item-title">Selamat Datang di Masjid Raden Patah</h5>
-          <h6 class="mrp-slider__item-subtitle">Universitas Brawijaya</h6>
-        </div>
-      </div>
-
-      <div class="mrp-slider__item">
-        <img src="{{ asset('img/carousel/footage_mesjid_7.JPG') }}" class="mrp-slider__item-image">
+        <img src="{{ asset('img/2.png') }}" class="mrp-slider__item-image">
         <div class="mrp-slider__item-desc">
           <h5 class="mrp-slider__item-title">Selamat Datang di Masjid Raden Patah</h5>
           <h6 class="mrp-slider__item-subtitle">Universitas Brawijaya</h6>
